b3c26267686ed531142911c1bc007c54fe0ab03d
Aymenfisher <fisheraymen@gmail.com>
2022-12-28 15:15:38 +0100

    projects page styled

--- src/features/projects/card/ProjectCard.js
+++ src/features/projects/card/ProjectCard.js
@@ -4,14 +4,20 @@ import './cardStyles.css'
 import { selectTechs } from '../../skills/skillsSlice';
 
 
-export const ProjectCard = () =>{
+export const ProjectCard = () => {
     const techsLogos = useSelector(selectTechs);
 
-    return(
+    return (
         <div className='card-container'>
-            <img className='project-screenshot' src='https://raw.githubusercontent.com/Aymenfisher/countries-app/master/src/design/desktop-design-detail-dark.jpg' alt='project screenshot' ></img>
+            <div className='project-screenshot'><img className='screenshot' src='https://raw.githubusercontent.com/Aymenfisher/countries-app/master/src/design/desktop-design-detail-dark.jpg' alt='project screenshot' ></img></div>
             <div className='card-body'>
-                <h2 className='project-title'>countries App</h2>
+                <div className='card-title'>
+                    <h2 className='project-title'>Countries App</h2>
+                    <div className='project-links'>
+                        <a href='https://www.github.com/aymenfisher' rel='noreferrer' target='_blank'><img className='project-link icon' src='https://svgshare.com/i/p54.svg' alt='Github Repo' title='Github Repo' /></a>
+                        <a href='https://www.github.com/aymenfisher' rel='noreferrer' target='_blank'><img className='project-link icon' src='https://svgshare.com/i/p7a.svg' alt='Live link' title='Live link' /></a>
+                    </div>
+                </div>
                 <div className='project-techs'>
                     <img className="skill-logo icon" src={techsLogos[0].logo} alt={'html logo'} title={'html'} key='html' />
                     <img className="skill-logo icon" src={techsLogos[1].logo} alt={'css logo'} title={'css'} key='css' />
--- src/features/projects/cards/ProjectsPage.js
+++ src/features/projects/cards/ProjectsPage.js
@@ -3,23 +3,26 @@ import { ProjectCard } from '../card/ProjectCard';
 import './projectsStyles.css'
 
 
-export const ProjectsPage = () =>{
+export const ProjectsPage = () => {
 
-    return(
+    return (
         <main className='projects'>
             <h2 className='projects-title'>Projects:</h2>
-            <div className='tech-projects'>
-                <ul className='cards'>
-                    <li className='card'><ProjectCard/></li>
-                    <li className='card'><ProjectCard/></li>
-                    <li className='card'><ProjectCard/></li>
-                    <li className='card'><ProjectCard/></li>
-                    <li className='card'><ProjectCard/></li>
-                    <li className='card'><ProjectCard/></li>
-                    <li className='card'><ProjectCard/></li>
-                    <li className='card'><ProjectCard/></li>
-                </ul>
-            </div>
+            <p className='projects-text '>
+                Here you can find my projects with their live link preview and Github repositories.<br />
+                I have worked in both Frontend and Backend sides. Implemented some projects from scartch, And completed parts of other projects.<br />
+                You can visit my Github Repositories <a style={{ color: 'steelblue' }} href='https://github.com/Aymenfisher?tab=repositories' rel='noreferrer' target='_blank'>Here.</a>
+            </p>
+            <ul className='cards tech-projects'>
+                <li className='card'><ProjectCard /></li>
+                <li className='card'><ProjectCard /></li>
+                <li className='card'><ProjectCard /></li>
+                <li className='card'><ProjectCard /></li>
+                <li className='card'><ProjectCard /></li>
+                <li className='card'><ProjectCard /></li>
+                <li className='card'><ProjectCard /></li>
+                <li className='card'><ProjectCard /></li>
+            </ul>
         </main>
     )
 }
